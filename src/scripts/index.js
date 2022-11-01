76c6070657acf94e12c9aba63d3ad8d79a42ebd8
Cyd Stumpel <info@cydstumpel.nl>
2022-11-01 10:30:20 +0100

    add gekke js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,3 +7,25 @@ if (headerTrigger) {
     headerList.classList.toggle('is-open')
   })
 }
+
+const body = document.querySelector('body')
+const contrastTrigger = document.querySelector('.header__contrast-trigger')
+
+if (contrastTrigger) {
+  contrastTrigger.addEventListener('click', () => {
+    body.classList.toggle('extra-contrast')
+  })
+}
+
+// const headerItems = document.querySelectorAll('a[href*="#"]')
+// if (headerItems) {
+//   headerItems.forEach((item) => {
+//     item.addEventListener('mouseenter', () => {
+//       console.log(item);
+//       item.classList.add('hover')
+//     })
+//     item.addEventListener('mouseleave', () => {
+//       item.classList.remove('hover')
+//     })
+//   })
+// }
